94f2e44c52ed42fa2fa3308640de18582e608c12
Philipp Mieden <dreadl0ck@protonmail.ch>
2019-05-02 10:59:22 +0200

    disabled package config for compilation, added debug output for failed OCIEnvCreate

--- globals.go
+++ globals.go
@@ -1,7 +1,6 @@
 package oci8
 
 /*
-#cgo !noPkgConfig pkg-config: oci8
 #include "oci8.go.h"
 */
 import "C"
@@ -12,6 +11,7 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"regexp"
@@ -157,13 +157,52 @@ func init() {
 	var envP *C.OCIEnv
 	envPP := &envP
 	var result C.sword
+
 	result = C.OCIEnvCreate(envPP, C.OCI_DEFAULT, nil, nil, nil, nil, 0, nil)
 	if result != C.OCI_SUCCESS {
-		panic("OCIEnvCreate error")
+		switch result {
+		case C.OCI_SUCCESS_WITH_INFO:
+			fmt.Printf("Error - OCI_SUCCESS_WITH_INFO\n")
+			//OCIErrorGet ((void  *) errhp, (ub4) 1, (text *) NULL, &errcode, errbuf, (ub4) sizeof(errbuf), (ub4) OCI_HTYPE_ERROR);
+			//fmt.Printf("Error - %s\n", errbuf)
+		case C.OCI_NEED_DATA:
+			fmt.Printf("Error - OCI_NEED_DATA\n")
+		case C.OCI_NO_DATA:
+			fmt.Printf("Error - OCI_NO_DATA\n")
+		case C.OCI_ERROR:
+			fmt.Printf("Error - OCI_ERROR\n")
+
+			fmt.Println([]byte(cGoStringN(cString("test"), 4)))
+			fmt.Println(cGoStringN(cString("test"), 4))
+
+			var errbuf = cStringN("", 1024)
+			fmt.Println(fmt.Sprint(C.OCIErrorGet(unsafe.Pointer(*envPP), 1, nil, &result, errbuf, 1024, C.OCI_HTYPE_ENV)))
+
+			//errorText := make([]byte, 512)
+			// fmt.Println(C.OCIErrorGet(unsafe.Pointer(*envPP), 1, nil, &result, (*C.OraText)(&errorText[0]), 512, C.OCI_HTYPE_ENV))
+
+			var msg = cGoStringN(errbuf, 512)
+			//index := bytes.IndexByte(errorText, 0)
+			//var msg = string(errorText[:index])
+
+			fmt.Println("ERROR MESSAGE:", msg)
+			//fmt.Println(hex.Dump([]byte(msg)))
+		case C.OCI_INVALID_HANDLE:
+			fmt.Printf("Error - OCI_INVALID_HANDLE\n")
+		case C.OCI_STILL_EXECUTING:
+			fmt.Printf("Error - OCI_STILL_EXECUTING\n")
+		case C.OCI_CONTINUE:
+			fmt.Printf("Error - OCI_CONTINUE\n")
+		default:
+			fmt.Printf("Error - %d\n", result)
+		}
+		panic("OCIEnvCreate error: " + fmt.Sprint(result))
 	}
+
 	nlsLang := cString("AL32UTF8")
 	defaultCharset = C.OCINlsCharSetNameToId(unsafe.Pointer(*envPP), (*C.oratext)(nlsLang))
 	C.free(unsafe.Pointer(nlsLang))
+
 	C.OCIHandleFree(unsafe.Pointer(*envPP), C.OCI_HTYPE_ENV)
 }
 
